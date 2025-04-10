921ac6d22440220c418c6bd108caf5e7dbd10fbc
Juuso Karjanlahti <98909830+kuxacodes@users.noreply.github.com>
2025-04-10 15:05:11 +0300

    Create SampleDataIntializer with  projects that have reviews

--- src/main/java/com/repophant/backend/domain/Review.java
+++ src/main/java/com/repophant/backend/domain/Review.java
@@ -2,10 +2,13 @@ package com.repophant.backend.domain;
 
 import java.time.Instant;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotBlank;
@@ -31,6 +34,8 @@ public class Review {
     private String poster;
 
     @ManyToOne
+    @JoinColumn(name = "project_id", nullable = false)
+    @JsonIgnore
     private Project project;
 
     private Instant postedAt;
