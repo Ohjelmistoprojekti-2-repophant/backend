570e6ac97d4a2944387f46bff803f7600389ba2b
Juuso Karjanlahti <98909830+juusokarjanlahti@users.noreply.github.com>
2025-04-11 23:18:33 +0300

    Configure SecurityConfig and UserController to require and retrieve GitHub access token for authenticated users.

--- src/main/java/com/repophant/backend/SecurityConfig.java
+++ src/main/java/com/repophant/backend/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -14,11 +15,19 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-
-        http.cors(Customizer.withDefaults())
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest().permitAll())
-                .oauth2Login(oauth2 -> oauth2.defaultSuccessUrl("http://localhost:5173/"));
+        http
+            .cors(Customizer.withDefaults())
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(authorizeRequests -> 
+                authorizeRequests
+                    .requestMatchers(new AntPathRequestMatcher("/api/github-token")).authenticated()
+                    .requestMatchers(new AntPathRequestMatcher("/api/user-info")).authenticated()
+                    .anyRequest().permitAll()
+            )
+            .oauth2Login(oauth2 -> 
+                oauth2
+                    .defaultSuccessUrl("http://localhost:5173/", true)
+            );
 
         return http.build();
     }
