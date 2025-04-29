53a091703a41ec502a398e632ecc529fa1566ccf
MarMakHH <bhi021@myy.haaga-helia.fi>
2025-04-29 10:43:58 +0300

    default url for oauth login as property variable

--- src/main/java/com/repophant/backend/config/SecurityConfig.java
+++ src/main/java/com/repophant/backend/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.repophant.backend.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -12,6 +13,9 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+  @Value("${defaultUrl}")
+  private String url;
 
   @Bean
   SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
@@ -27,7 +31,7 @@ public class SecurityConfig {
                     .authenticated()
                     .anyRequest()
                     .permitAll())
-        .oauth2Login(oauth2 -> oauth2.defaultSuccessUrl("http://localhost:5173", true));
+        .oauth2Login(oauth2 -> oauth2.defaultSuccessUrl(url, true));
 
     return http.build();
   }
